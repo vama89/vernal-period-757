0467da038ebdde2eeaf61fa8124654e1e84e06b3
Vincent Arrogancia <varrogancia@gmail.com>
2015-07-10 15:17:34 -0700

    fixed the login and registration and added friendButton function

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -224,6 +224,69 @@ conferenceApp.controllers.controller('HangoutCreationCtrl', function ($scope, $l
 
     $scope.checked = $scope.checked || {};
 
+    $scope.friendButton = function(hangoutForm) {
+        $(document).ready(function() {
+        var iCnt = 0;
+        // CREATE A "DIV" ELEMENT AND DESIGN IT USING JQUERY ".css()" CLASS.
+        var container = $(document.createElement('div')).css({
+            padding: '5px', margin: '20px', width: '170px',
+            borderTopColor: '#999', borderBottomColor: '#999',
+            borderLeftColor: '#999', borderRightColor: '#999'
+        });
+        $('#btAdd').click(function() {
+            if (iCnt <= 50) {
+                iCnt = iCnt + 1;
+                // ADD TEXTBOX.
+                $(container).prepend('<input type=text class="input" id=tb' + iCnt + ' ' +
+                            'name="friend' + iCnt + '" placeholder="Friend ' + iCnt +'"'+'required/>');
+
+                if (iCnt == 100) {        // SHOW SUBMIT BUTTON IF ATLEAST "1" ELEMENT HAS BEEN CREATED.
+                    var divSubmit = $(document.createElement('div'));
+                    $(divSubmit).append('<input type=button class="bt" onclick="GetTextValue()"' + 
+                            'id=btSubmit value=Submit />');   
+                }
+                $('#main').after(container, divSubmit);   // ADD BOTH THE DIV ELEMENTS TO THE "main" CONTAINER.
+            }
+            else {      // AFTER REACHING THE SPECIFIED LIMIT, DISABLE THE "ADD" BUTTON. (20 IS THE LIMIT WE HAVE SET)
+                $(container).append('<label>Reached the limit</label>'); 
+                $('#btAdd').attr('class', 'bt-disable'); 
+                $('#btAdd').attr('disabled', 'disabled');
+            }
+        });
+        $('#btRemove').click(function() {   // REMOVE ELEMENTS ONE PER CLICK.
+            if (iCnt != 0) { $('#tb' + iCnt).remove(); iCnt = iCnt - 1; }
+            if (iCnt == 0) { $(container).empty(); 
+                $(container).remove(); 
+                $('#btSubmit').remove(); 
+                $('#btAdd').removeAttr('disabled'); 
+                $('#btAdd').attr('class', 'bt') 
+            }
+        });
+        $('#btRemoveAll').click(function() {    // REMOVE ALL THE ELEMENTS IN THE CONTAINER.
+            $(container).empty(); 
+            $(container).remove(); 
+            $('#btSubmit').remove(); iCnt = 0; 
+            $('#btAdd').removeAttr('disabled'); 
+            $('#btAdd').attr('class', 'bt');
+        });
+    });
+
+    // PICK THE VALUES FROM EACH TEXTBOX WHEN "SUBMIT" BUTTON IS CLICKED.
+    var divValue, values = '';
+    function GetTextValue() {
+        $(divValue).empty(); 
+        $(divValue).remove(); values = '';
+        $('.input').each(function() {
+            divValue = $(document.createElement('div')).css({
+                padding:'5px', width:'200px'
+            });
+            values += this.value + '<br />'
+        });
+        $(divValue).append('<p><b>Your selected values</b></p>' + values);
+        $('body').append(divValue);
+    }
+    };
+
     $scope.allOptionsChecked = function(hangoutForm) {
         if ($scope.checked.location && $scope.checked.time && $scope.checked.day) {
             return true;
